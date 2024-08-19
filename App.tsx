fd76de15c5b709b3c842534bc41c24bee79792a3
Tushar Dahiya <shreyashsam@gmail.com>
2024-08-20 00:09:49 +0530

    added shopping list item

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, PixelRatio } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.itemContainer}>
+                <Text style={styles.itemText}>
+                {PixelRatio.get()} Coffee
+                </Text>
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+    },
+    itemContainer: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#1a759f",
+        paddingHorizontal: 8,
+        paddingVertical: 16
+    },
+    itemText: {
+        fontSize: 18,
+        fontWeight: "200",
+    },
 });
