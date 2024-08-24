ce289b5debf9d0e67e8d74e5df211f9be87abda1
Tushar Dahiya <shreyashsam@gmail.com>
2024-08-25 03:26:06 +0530

    added a very big delete button

--- App.tsx
+++ App.tsx
@@ -1,31 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, PixelRatio } from "react-native";
+import { StyleSheet, Text, Alert, View, PixelRatio, Button, TouchableOpacity, Pressable } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>{PixelRatio.get()} Coffee</Text>
-        <StatusBar style="auto" />
-      </View>
-    </View>
-  );
+    const handleDelete = () => {
+        Alert.alert(
+            "Are you sure u want to delete this?",
+            "it will be gone for good!",
+            [
+                {
+                    text: "yes",
+                    onPress: () => console.log("ok, deleted"),
+                    style: "destructive",
+                },
+                {
+                    text: "cancel",
+                    onPress: () => console.log("cancelled"),
+                    style: "cancel",
+                },
+            ]
+        );
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.itemContainer}>
+                <Text style={styles.itemText}>Coffee</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleDelete}
+                    activeOpacity={0.1}>
+                    <Text
+                        style={styles.buttonText}
+                    >Delete</Text>
+                </TouchableOpacity>
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
-    paddingHorizontal: 8,
-    paddingVertical: 16,
-  },
-  itemText: {
-    fontSize: 18,
-    fontWeight: "200",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+    },
+    itemContainer: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#1a759f",
+        paddingHorizontal: 8,
+        paddingVertical: 16,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    itemText: {
+        fontSize: 48,
+        fontWeight: "200",
+    },
+    button: {
+        backgroundColor: theme.colorBlack,
+        borderRadius: 15,
+        padding: 10,
+    },
+    buttonText: {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        color: theme.colorWhite,
+        fontSize: 38,
+        letterSpacing: 1,
+    }
 });
